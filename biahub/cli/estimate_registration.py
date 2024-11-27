-adding voxel scale to estimate stabilization and stabilize. Fixes scaling mismatch - Put the voxel_size in the config. It's easier than adding CLI argument

biahub/cli/estimate_registration.py
@@ -1,14 +1,17 @@
+from functools import partial
+from multiprocessing import Pool
+
 import ants
 import click
+import dask.array as da
 import napari
 import numpy as np
-import dask.array as da
 
 from iohub import open_ome_zarr
 from iohub.reader import print_info
-from skimage.transform import EuclideanTransform, SimilarityTransform, AffineTransform
-from skimage.feature import match_descriptors
 from scipy.interpolate import interp1d
+from skimage.feature import match_descriptors
+from skimage.transform import AffineTransform, EuclideanTransform, SimilarityTransform
 from waveorder.focus import focus_from_transverse_band
 
 from biahub.analysis.AnalysisSettings import RegistrationSettings, StabilizationSettings
@@ -20,14 +23,12 @@ from biahub.analysis.register import (
     get_3D_rotation_matrix,
 )
 from biahub.cli.parsing import (
+    num_processes,
     output_filepath,
     source_position_dirpaths,
     target_position_dirpaths,
-    num_processes,
 )
-from biahub.cli.utils import model_to_yaml, _check_nan_n_zeros
-from multiprocessing import Pool
-from functools import partial
+from biahub.cli.utils import _check_nan_n_zeros, model_to_yaml
 
 # TODO: see if at some point these globals should be hidden or exposed.
 NA_DETECTION_SOURCE = 1.35
@@ -47,10 +48,17 @@ COLOR_CYCLE = [
     "magenta",
 ]
 
+# APPROX_TFORM = [
+#     [1, 0, 0, 0],
+#     [0, 0, -1.288, 1960],
+#     [0, 1.288, 0, -460],
+#     [0, 0, 0, 1],
+# ]
+
 APPROX_TFORM = [
     [1, 0, 0, 0],
-    [0, 0, -1.288, 1960],
-    [0, 1.288, 0, -460],
+    [0, 0, -1.288, 1498],
+    [0, 1.29, 0, -165],
     [0, 0, 0, 1],
 ]
 
@@ -323,7 +331,7 @@ def beads_based_registration(
     T = source_channel_tzyx.shape[0]
     x, y = zip(*[(i, transforms[i]) for i in range(T) if transforms[i] is not None])
     f = interp1d(x, y, axis=0, kind="linear", fill_value="extrapolate")
-    
+
     return f(range(T)).tolist()
 
 
@@ -341,14 +349,15 @@ def _get_tform_from_beads(
 
     source_data_ants = ants.from_numpy(source_channel_zyx)
     target_data_ants = ants.from_numpy(target_channel_zyx)
-    source_data_reg = convert_transform_to_ants(approx_tform).apply_to_image(
-        source_data_ants, reference=target_data_ants
-    ).numpy()
-
+    source_data_reg = (
+        convert_transform_to_ants(approx_tform)
+        .apply_to_image(source_data_ants, reference=target_data_ants)
+        .numpy()
+    )
     source_peaks = detect_peaks(
         source_data_reg,
         block_size=[32, 16, 16],
-        threshold_abs=110,
+        threshold_abs=np.percentile(source_data_reg, 99.98),
         nms_distance=16,
         min_distance=0,
         verbose=True,
@@ -361,6 +370,11 @@ def _get_tform_from_beads(
         min_distance=0,
         verbose=True,
     )
+
+    # Skip if there is no peak detected
+    if len(source_peaks) < 2 or len(target_peaks) < 2:
+        click.echo('No peak detected')
+        return
 
     # Match peaks, excluding top 5% of distances as outliers
     matches = match_descriptors(source_peaks, target_peaks, metric="euclidean")
@@ -389,14 +403,26 @@ def _get_tform_from_beads(
     is_flag=True,
     help='Flag to use similarity transform (rotation, translation, scaling) default:Eucledian (rotation, translation)',
 )
-@click.option("--t_idx", type=int, required=False, default=0, help="Time index to use for registration estimation")
+@click.option(
+    "--t_idx",
+    type=int,
+    required=False,
+    default=0,
+    help="Time index to use for registration estimation",
+)
 @click.option(
     "--beads",
     is_flag=True,
     help="Flag to estimate registration parameters based on bead images",
 )
 def estimate_registration(
-    source_position_dirpaths, target_position_dirpaths, output_filepath, num_processes, similarity, t_idx, beads
+    source_position_dirpaths,
+    target_position_dirpaths,
+    output_filepath,
+    num_processes,
+    similarity,
+    t_idx,
+    beads,
 ):
     """
     Estimate the affine transform between a source (i.e. moving) and a target (i.e.
@@ -426,13 +452,18 @@ def estimate_registration(
     with open_ome_zarr(source_position_dirpaths[0], mode="r") as source_channel_position:
         source_channels = source_channel_position.channel_names
         source_channel_name = source_channels[source_channel_index]
-        source_channel_data = da.from_zarr(source_channel_position.data)[:, source_channel_index]
+        source_channel_data = da.from_zarr(source_channel_position.data)[
+            :, source_channel_index
+        ]
         source_channel_voxel_size = source_channel_position.scale[-3:]
 
     with open_ome_zarr(target_position_dirpaths[0], mode="r") as target_channel_position:
         target_channel_name = target_channel_position.channel_names[target_channel_index]
-        target_channel_data = da.from_zarr(target_channel_position.data)[:, target_channel_index]
-        target_channel_voxel_size = target_channel_position.scale[-3:]
+        target_channel_data = da.from_zarr(target_channel_position.data)[
+            :, target_channel_index
+        ]
+        voxel_size = target_channel_position.scale
+        target_channel_voxel_size = voxel_size[-3:]
 
     additional_source_channels = source_channels.copy()
     additional_source_channels.remove(source_channel_name)
@@ -466,6 +497,7 @@ def estimate_registration(
             stabilization_channels=source_channel_names,
             affine_transform_zyx_list=transforms,
             time_indices='all',
+            voxel_size=voxel_size,
         )
     else:
         # Register based on user input

biahub/cli/stabilize.py
@@ -109,7 +109,7 @@ def stabilize(input_position_dirpaths, output_dirpath, config_filepath, num_proc
     output_metadata = {
         "shape": (len(time_indices), len(channel_names), Z, Y, X),
         "chunks": None,
-        "scale": dataset.scale,
+        "scale": settings.voxel_size,
         "channel_names": channel_names,
         "dtype": np.float32,
     }