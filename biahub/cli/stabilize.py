1d444c7c767e27181822a809de7e0c9781779272
Taylla Theodoro <tayllamth@gmail.com>
2024-12-17 14:33:39 -0800

    stabilize cli with submitit

--- biahub/cli/stabilize.py
+++ biahub/cli/stabilize.py
@@ -1,12 +1,23 @@
+from pathlib import Path
+from typing import List
+
 import ants
 import click
 import numpy as np
+import submitit
 
 from iohub.ngff import open_ome_zarr
 
 from biahub.analysis.AnalysisSettings import StabilizationSettings
 from biahub.analysis.register import convert_transform_to_ants
-from biahub.cli.parsing import config_filepath, input_position_dirpaths, output_dirpath
+from biahub.cli.parsing import (
+    config_filepath,
+    input_position_dirpaths,
+    local,
+    output_dirpath,
+    sbatch_filepath,
+    sbatch_to_submitit,
+)
 from biahub.cli.utils import (
     copy_n_paste_czyx,
     create_empty_hcs_zarr,
@@ -52,15 +63,15 @@ def apply_stabilization_transform(
 @input_position_dirpaths()
 @output_dirpath()
 @config_filepath()
-@click.option(
-    "--num-processes",
-    "-j",
-    default=1,
-    help="Number of parallel processes. Default is 1.",
-    required=False,
-    type=int,
-)
-def stabilize(input_position_dirpaths, output_dirpath, config_filepath, num_processes):
+@sbatch_filepath()
+@local()
+def stabilize(
+    input_position_dirpaths: List[str],
+    output_dirpath: str,
+    config_filepath: str,
+    sbatch_filepath: str = None,
+    local: bool = False,
+):
     """
     Stabilize the timelapse input based on single position and channel.
 
@@ -74,6 +85,10 @@ def stabilize(input_position_dirpaths, output_dirpath, config_filepath, num_proc
     """
     assert config_filepath.suffix == ".yml", "Config file must be a yaml file"
 
+    # Convert to Path objects
+    config_filepath = Path(config_filepath)
+    output_dirpath = Path(output_dirpath)
+    slurm_out_path = output_dirpath.parent / "slurm_output"
     # Load the config file
     settings = yaml_to_model(config_filepath, StabilizationSettings)
 
@@ -84,7 +99,6 @@ def stabilize(input_position_dirpaths, output_dirpath, config_filepath, num_proc
     with open_ome_zarr(input_position_dirpaths[0]) as dataset:
         T, C, Z, Y, X = dataset.data.shape
         channel_names = dataset.channel_names
-        scale_dataset = dataset.scale
         for channel in stabilization_channels:
             if channel not in channel_names:
                 raise ValueError(f"Channel <{channel}> not found in the input data")
@@ -135,31 +149,72 @@ def stabilize(input_position_dirpaths, output_dirpath, config_filepath, num_proc
     stabilize_zyx_args = {"list_of_shifts": combined_mats}
     copy_n_paste_kwargs = {"czyx_slicing_params": ([Z_slice, Y_slice, X_slice])}
 
-    # apply stabilization to channels in the chosen channels and else copy the rest
-    for input_position_path in input_position_dirpaths:
-        for channel_name in channel_names:
-            if channel_name in stabilization_channels:
-                process_single_position_v2(
-                    apply_stabilization_transform,
-                    input_data_path=input_position_path,  # source store
-                    output_path=output_dirpath,
-                    time_indices=time_indices,
-                    input_channel_idx=[channel_names.index(channel_name)],
-                    output_channel_idx=[channel_names.index(channel_name)],
-                    num_processes=num_processes,  # parallel processing over time
-                    **stabilize_zyx_args,
-                )
-            else:
-                process_single_position_v2(
-                    copy_n_paste_czyx,
-                    input_data_path=input_position_path,  # target store
-                    output_path=output_dirpath,
-                    time_indices=time_indices,
-                    input_channel_idx=[channel_names.index(channel_name)],
-                    output_channel_idx=[channel_names.index(channel_name)],
-                    num_processes=num_processes,
-                    **copy_n_paste_kwargs,
-                )
+    # Estimate resources
+    gb_per_element = 4 / 2**30  # bytes_per_float32 / bytes_per_gb
+    num_cpus = np.min([T * C, 16])
+    input_memory = num_cpus * Z * Y * X * gb_per_element
+    gb_ram_request = np.ceil(np.max([1, input_memory])).astype(int)
+
+    # Prepare SLURM arguments
+    slurm_args = {
+        "slurm_job_name": "stabilize",
+        "slurm_mem_per_cpu": f"{gb_ram_request}G",
+        "slurm_cpus_per_task": num_cpus,
+        "slurm_array_parallelism": 100,  # process up to 100 positions at a time
+        "slurm_time": 60,
+        "slurm_partition": "preempted",
+    }
+
+    # Override defaults if sbatch_filepath is provided
+    if sbatch_filepath:
+        slurm_args.update(sbatch_to_submitit(sbatch_filepath))
+
+    # Run locally or submit to SLURM
+    if local:
+        cluster = "local"
+    else:
+        cluster = "slurm"
+
+    # Prepare and submit jobs
+    click.echo(f"Preparing jobs: {slurm_args}")
+    executor = submitit.AutoExecutor(folder=slurm_out_path, cluster=cluster)
+    executor.update_parameters(**slurm_args)
+
+    click.echo('Submitting SLURM jobs...')
+    jobs = []
+
+    with executor.batch():
+        # apply stabilization to channels in the chosen channels and else copy the rest
+        for input_position_path in input_position_dirpaths:
+            for channel_name in channel_names:
+                if channel_name in stabilization_channels:
+                    job = executor.submit(
+                        process_single_position_v2,
+                        apply_stabilization_transform,
+                        input_data_path=input_position_path,  # source store
+                        output_path=output_dirpath,
+                        time_indices=time_indices,
+                        input_channel_idx=[channel_names.index(channel_name)],
+                        output_channel_idx=[channel_names.index(channel_name)],
+                        num_processes=int(
+                            slurm_args["slurm_cpus_per_task"]
+                        ),  # parallel processing over time
+                        **stabilize_zyx_args,
+                    )
+                else:
+                    job = executor.submit(
+                        process_single_position_v2,
+                        copy_n_paste_czyx,
+                        input_data_path=input_position_path,  # target store
+                        output_path=output_dirpath,
+                        time_indices=time_indices,
+                        input_channel_idx=[channel_names.index(channel_name)],
+                        output_channel_idx=[channel_names.index(channel_name)],
+                        num_processes=int(slurm_args["slurm_cpus_per_task"]),
+                        **copy_n_paste_kwargs,
+                    )
+
+                jobs.append(job)
 
 
 if __name__ == "__main__":
