ebab144f95c7a34d8f420cc4119870e45ada349f
Taylla Theodoro <tayllamth@gmail.com>
2025-01-23 10:06:18 -0800

    use estimate_resources

--- biahub/cli/stabilize.py
+++ biahub/cli/stabilize.py
@@ -25,6 +25,7 @@ from biahub.cli.utils import (
     create_empty_hcs_zarr,
     process_single_position_v2,
     yaml_to_model,
+    estimate_resources
 )
 
 
@@ -170,15 +171,13 @@ def stabilize(
     copy_n_paste_kwargs = {"czyx_slicing_params": ([Z_slice, Y_slice, X_slice])}
 
     # Estimate resources
-    gb_per_element = 4 / 2**30  # bytes_per_float32 / bytes_per_gb
-    num_cpus = np.min([T * C, 16])
-    input_memory = num_cpus * Z * Y * X * gb_per_element
-    gb_ram_request = np.ceil(np.max([1, input_memory])).astype(int)
+    
+    num_cpus, gb_ram_per_cpu = estimate_resources(shape=[T,C,Z,Y,X])
 
     # Prepare SLURM arguments
     slurm_args = {
         "slurm_job_name": "stabilize",
-        "slurm_mem_per_cpu": f"{gb_ram_request}G",
+        "slurm_mem_per_cpu": f"{gb_ram_per_cpu}G",
         "slurm_cpus_per_task": num_cpus,
         "slurm_array_parallelism": 100,  # process up to 100 positions at a time
         "slurm_time": 60,
