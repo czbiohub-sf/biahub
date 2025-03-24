37a64f5f680aefcc3f1ab3b87b388edfd8135384
Ivan Ivanov <ivan.ivanov@czbiohub.org>
2025-03-23 20:07:12 -0700

    move estimate_registration

--- biahub/cli/main.py
+++ biahub/cli/main.py
@@ -2,7 +2,6 @@ import click
 
 from biahub.cli.characterize_psf import characterize_psf
 from biahub.cli.estimate_psf import estimate_psf
-from biahub.cli.estimate_registration import estimate_registration
 from biahub.cli.optimize_registration import optimize_registration
 from biahub.cli.stabilize import stabilize
 from biahub.concatenate import concatenate_cli
@@ -10,6 +9,7 @@ from biahub.deconvolve import deconvolve_cli
 from biahub.deskew import deskew_cli
 from biahub.estimate_bleaching import estimate_bleaching_cli
 from biahub.estimate_deskew import estimate_deskew_cli
+from biahub.estimate_registration import estimate_registration_cli
 from biahub.estimate_stabilization import estimate_stabilization_cli
 from biahub.estimate_stitch import estimate_stitch_cli
 from biahub.register import register_cli
@@ -33,7 +33,7 @@ def cli():
 cli.add_command(estimate_bleaching_cli)
 cli.add_command(estimate_deskew_cli)
 cli.add_command(deskew_cli)
-cli.add_command(estimate_registration)
+cli.add_command(estimate_registration_cli)
 cli.add_command(optimize_registration)
 cli.add_command(register_cli)
 cli.add_command(estimate_stitch_cli)
