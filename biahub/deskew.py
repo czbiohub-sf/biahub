952e75f562e2b7831f2800e1cf7eb9fa08c1ff7c
Ivan Ivanov <ivan.ivanov@czbiohub.org>
2025-04-14 11:42:58 -0700

    style

--- biahub/deskew.py
+++ biahub/deskew.py
@@ -307,7 +307,9 @@ def deskew_cli(
     }
 
     # Estimate resources
-    num_cpus, gb_ram = estimate_resources(shape=(T, C, Z, Y, X), ram_multiplier=10, max_num_cpus=16)
+    num_cpus, gb_ram = estimate_resources(
+        shape=(T, C, Z, Y, X), ram_multiplier=10, max_num_cpus=16
+    )
 
     # Prepare SLURM arguments
     slurm_args = {
