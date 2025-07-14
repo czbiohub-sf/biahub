1709def03ea669add5da70f1735ecc2e89c17850
Ivan Ivanov <ivan.ivanov@czbiohub.org>
2025-07-14 16:04:09 -0700

    refactor process_w_imports

--- biahub/process_data.py
+++ biahub/process_data.py
@@ -17,7 +17,7 @@ from biahub.cli.parsing import (
     sbatch_filepath,
     sbatch_to_submitit,
 )
-from biahub.cli.utils import get_output_paths, yaml_to_model
+from biahub.cli.utils import estimate_resources, get_output_paths, yaml_to_model
 from biahub.settings import ProcessingFunctions, ProcessingImportFuncSettings
 
 
@@ -130,34 +130,30 @@ def process_czyx(
     return czyx_data
 
 
-@click.command()
-@input_position_dirpaths()
-@config_filepath()
-@output_dirpath()
-@sbatch_filepath()
-@local()
-@click.option(
-    "--num-processes",
-    "-j",
-    default=1,
-    help="Number of parallel processes. Default is 1.",
-    required=False,
-    type=int,
-)
-def process_w_imports_cli(
-    input_position_dirpaths,
-    config_filepath,
-    output_dirpath,
-    sbatch_filepath,
-    local,
-    num_processes,
-):
+def process_w_imports(
+    input_position_dirpaths: Sequence[Path],
+    config_filepath: Path,
+    output_dirpath: Path,
+    sbatch_filepath: Path | None = None,
+    local: bool = False,
+) -> None:
     """
     Process data with functions specified in the config file.
 
-    Example usage:
-    biahub process-w-imports -i ./timelapse.zarr/0/0/0 -c ./process_params.yml -o ./processed_timelapse.zarr -j 1
+    Parameters
+    ----------
+    input_position_dirpaths : Sequence[Path]
+        Input position directory paths
+    config_filepath : Path
+        Path to the configuration file
+    output_dirpath : Path
+        Output directory path
+    sbatch_filepath : Path | None, optional
+        Path to the SLURM batch file, by default None
+    local : bool, optional
+        Whether to run locally or submit to SLURM, by default False
     """
+
     # Convert to Path objects
     output_dirpath = Path(output_dirpath)
     config_filepath = Path(config_filepath)
@@ -170,50 +166,48 @@ def process_w_imports_cli(
         channel_names = dataset.channel_names
         scale_dataset = dataset.scale
 
-        settings = yaml_to_model(config_filepath, ProcessingImportFuncSettings)
+    settings = yaml_to_model(config_filepath, ProcessingImportFuncSettings)
 
-        if settings.processing_functions is not None:
-            for proc in settings.processing_functions:
-                # Replace the channel name with the channel index
-                if proc.channel is not None:
-                    proc.channel = channel_names.index(proc.channel)
-                else:
-                    raise ValueError("Channel must be specified for preprocessing functions")
-        else:
-            raise ValueError("Processing functions must be specified")
+    if settings.processing_functions is not None:
+        for proc in settings.processing_functions:
+            # Replace the channel name with the channel index
+            if proc.channel is not None:
+                proc.channel = channel_names.index(proc.channel)
+            else:
+                raise ValueError("Channel must be specified for preprocessing functions")
+    else:
+        raise ValueError("Processing functions must be specified")
 
-        # FIXME: temporary for binning functions
-        if any(
-            proc.function.__name__ == "binning_czyx" for proc in settings.processing_functions
-        ):
-            click.echo("Binning output shape is hard")
-            # Get the binning factor from the first binning function found
-            for proc in settings.processing_functions:
-                if proc.function.__name__ == "binning_czyx":
-                    binning_factor = proc.kwargs.get("binning_factor_zyx", (1, 4, 4))
-                    click.echo(f"Binning factor: {binning_factor}")
-                    break
+    # FIXME: temporary for binning functions
+    if any(proc.function.__name__ == "binning_czyx" for proc in settings.processing_functions):
+        click.echo("Binning output shape is hard")
+        # Get the binning factor from the first binning function found
+        for proc in settings.processing_functions:
+            if proc.function.__name__ == "binning_czyx":
+                binning_factor = proc.kwargs.get("binning_factor_zyx", (1, 4, 4))
+                click.echo(f"Binning factor: {binning_factor}")
+                break
 
-            # Calculate new dimensions after binning
-            new_Z = Z // binning_factor[0]
-            new_Y = Y // binning_factor[1]
-            new_X = X // binning_factor[2]
+        # Calculate new dimensions after binning
+        new_Z = Z // binning_factor[0]
+        new_Y = Y // binning_factor[1]
+        new_X = X // binning_factor[2]
 
-            output_shape = (T, C, new_Z, new_Y, new_X)
-            new_scale = [
-                scale_dataset[0],  # T
-                scale_dataset[1],  # C
-                scale_dataset[2] * binning_factor[0],  # Z
-                scale_dataset[3] * binning_factor[1],  # Y
-                scale_dataset[4] * binning_factor[2],  # X
-            ]
-        else:
-            # Calculate output shape based on processing functions
-            # For now, we'll assume the shape stays the same
-            # TODO: Add logic to calculate output shape based on processing functions
+        output_shape = (T, C, new_Z, new_Y, new_X)
+        new_scale = [
+            scale_dataset[0],  # T
+            scale_dataset[1],  # C
+            scale_dataset[2] * binning_factor[0],  # Z
+            scale_dataset[3] * binning_factor[1],  # Y
+            scale_dataset[4] * binning_factor[2],  # X
+        ]
+    else:
+        # Calculate output shape based on processing functions
+        # For now, we'll assume the shape stays the same
+        # TODO: Add logic to calculate output shape based on processing functions
 
-            output_shape = (T, C, Z, Y, X)
-            new_scale = scale_dataset
+        output_shape = (T, C, Z, Y, X)
+        new_scale = scale_dataset
 
     # TODO: should the channels be the same as the input channels?
     output_metadata = {
@@ -236,19 +230,17 @@ def process_w_imports_cli(
     }
 
     # Estimate Resources
-    gb_per_element = 4 / 2**30  # bytes_per_float32 / bytes_per_gb
-    num_cpus = np.min([T * C, 16])
-    input_memory = num_cpus * Z * Y * X * gb_per_element
-    gb_ram_request = np.ceil(np.max([1, input_memory])).astype(int)
-    slurm_time = np.ceil(np.max([60, T * 0.75])).astype(int)
+    num_cpus, gb_ram_per_cpu = estimate_resources(
+        shape=output_shape, dtype=np.float32, ram_multiplier=4, max_num_cpus=16
+    )
 
     # Prepare SLURM arguments
     slurm_args = {
         "slurm_job_name": "process_data",
-        "slurm_mem_per_cpu": f"{gb_ram_request}G",
+        "slurm_mem_per_cpu": f"{gb_ram_per_cpu}G",
         "slurm_cpus_per_task": num_cpus,
         "slurm_array_parallelism": 100,  # process up to 100 positions at a time
-        "slurm_time": slurm_time,
+        "slurm_time": 60,
         "slurm_partition": "cpu",
     }
 
@@ -267,7 +259,7 @@ def process_w_imports_cli(
     executor = submitit.AutoExecutor(folder=slurm_out_path, cluster=cluster)
     executor.update_parameters(**slurm_args)
 
-    # TODO: perhaps these should be exposed as parameters in config
+    # TODO: perhaps T/C processing indices should be exposed as params in config
     jobs = []
     with executor.batch():
         for input_position_path, output_position_path in zip(
@@ -282,12 +274,40 @@ def process_w_imports_cli(
                     input_time_indices=list(range(T)),
                     input_channel_indices=[list(range(C))],  # Process all channels
                     output_channel_indices=[list(range(C))],
-                    num_processes=np.max([1, num_cpus - 3]),
+                    num_processes=slurm_args["slurm_cpus_per_task"],
                     **process_args,
                 )
             )
 
     monitor_jobs(jobs, input_position_dirpaths)
+
+
+@click.command("process-w-imports")
+@input_position_dirpaths()
+@config_filepath()
+@output_dirpath()
+@sbatch_filepath()
+@local()
+def process_w_imports_cli(
+    input_position_dirpaths,
+    config_filepath,
+    output_dirpath,
+    sbatch_filepath,
+    local,
+):
+    """
+    Process data with functions specified in the config file.
+
+    Example usage:
+    biahub process-w-imports -i ./timelapse.zarr/0/0/0 -c ./process_params.yml -o ./processed_timelapse.zarr -j 1
+    """
+    process_w_imports(
+        input_position_dirpaths=input_position_dirpaths,
+        config_filepath=config_filepath,
+        output_dirpath=output_dirpath,
+        sbatch_filepath=sbatch_filepath,
+        local=local,
+    )
 
 
 if __name__ == "__main__":
