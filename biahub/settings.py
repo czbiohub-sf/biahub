4aef3d86dceaf2e615c7bd64bba321899c59d188
Taylla Milena Theodoro <tayllamth@gmail.com>
2025-07-14 15:39:56 -0700

    Add SLURM-enabled `biahub track` CLI for Ultrack-based cell tracking (#93)
    * add ultrack as dependency
    
    * yml model
    
    * track cli
    
    * call of track cli
    
    * update model to read ultrack config and check nan to check empty frames
    
    * example settings for track cli
    
    * style fix
    
    * read blank frame csv
    
    * style fix
    
    * documentation fix
    
    * style fix
    
    * added max_num_cpu for slurm
    
    * cli pattern
    
    * Update biahub/track.py
    
    Co-authored-by: Jordão Bragantini <jordao.bragantini@gmail.com>
    
    * match n_workers with n_cpus
    
    * adding 3d tracking
    
    * update ultrack call
    
    * working v3
    
    * ultrack tag version
    
    * bug fix
    
    * bug fix
    
    * comment example
    
    * update partition
    
    * add max_cpu for concatenate
    
    * path from config
    
    * style
    
    * drop duplicate print
    
    * update slurm arg
    
    * remove read_data function
    
    * rename run_pipeline
    
    * rename z_slice to z_range
    
    * style
    
    * filename fix
    
    * update to create_empty_plate from iohub
    
    * return _check_nan_n_zeros to main and add get_empty_frame_indices
    
    * update resolve_z_slice
    
    * style
    
    * add tests
    
    * documentation on config
    
    * return in track one position
    
    * debug tracking
    
    * improve load data
    
    * correct test
    
    * style
    
    * add documentation
    
    * rename script
    
    * update documentation
    
    * call track function instead of cli for test
    
    ---------
    
    Co-authored-by: Jordão Bragantini <jordao.bragantini@gmail.com>
    Co-authored-by: Ivan Ivanov <ivan.ivanov@czbiohub.org>

--- biahub/settings.py
+++ biahub/settings.py
@@ -1,10 +1,12 @@
 import warnings
 
-from typing import Any, Dict, Literal, Optional, Union
+from pathlib import Path
+from typing import Any, Dict, List, Literal, Optional, Tuple, Union
 
 import numpy as np
 import torch
 
+from iohub import open_ome_zarr
 from pydantic import (
     BaseModel,
     ConfigDict,
@@ -21,6 +23,50 @@ from pydantic import (
 # All settings classes inherit from MyBaseModel, which forbids extra parameters to guard against typos
 class MyBaseModel(BaseModel):
     model_config = ConfigDict(extra="forbid")
+
+
+class ProcessingFunctions(MyBaseModel):
+    function: str
+    input_channels: Optional[List[str]] = None  # Optional
+    kwargs: Dict[str, Any] = {}
+    per_timepoint: bool = True
+
+
+class ProcessingInputChannel(MyBaseModel):
+    path: Union[str, None] = None
+    channels: Dict[str, List[ProcessingFunctions]]
+
+    @field_validator("path")
+    @classmethod
+    def validate_path_not_plate(cls, v):
+        if v is None:
+            return v
+        try:
+            v = Path(v)
+            with open_ome_zarr(v, mode='r'):
+                pass
+        except Exception as e:
+            print(e)
+            raise ValueError(f"Path {v} is not a valid OME-Zarr path")
+
+        return v
+
+
+class TrackingSettings(MyBaseModel):
+    target_channel: str = "nuclei_prediction"
+    fov: str = "*/*/*"
+    blank_frames_path: str = None
+    mode: Literal["2D", "3D"] = "2D"
+    z_range: Optional[Tuple[int, int]] = None
+    input_images: List[ProcessingInputChannel]
+    tracking_config: Dict[str, Any] = {}
+
+    @field_validator("blank_frames_path")
+    @classmethod
+    def validate_blank_frames_path(cls, v):
+        if v is None:
+            return v
+        return Path(v)
 
 
 class EstimateRegistrationSettings(MyBaseModel):
