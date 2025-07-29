5abf6b1908efdffbaab3e317d5a90f89bd9766a8
Taylla Theodoro <tayllamth@gmail.com>
2025-07-29 14:44:17 -0700

    correct deskew

--- biahub/deskew.py
+++ biahub/deskew.py
@@ -312,7 +312,7 @@ def deskew_cli(
 
     # Estimate resources
     num_cpus, gb_ram = estimate_resources(
-        shape=(T, C, Z, Y, X), ram_multiplier=10, max_num_cpus=16
+        shape=(T, C, Z, Y, X), ram_multiplier=16, max_num_cpus=16
     )
 
     # Prepare SLURM arguments
