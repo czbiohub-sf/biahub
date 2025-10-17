f0a68607c5ef7a91d2f4157a184ba1fc0cdbe90b
Sricharan Reddy Varra <sricharan.varra@czbiohub.org>
2025-10-17 10:47:06 -0700

    added napari-psf-analysis vendored version

--- biahub/vendor/napari_psf_analysis/__init__.py
+++ biahub/vendor/napari_psf_analysis/__init__.py
@@ -1,3 +1,9 @@
+"""
+Vendored subset of napari_psf_analysis (v1.1.4).
+
+This module contains selected components from the napari_psf_analysis package.
+"""
+
 from .image import Calibrated3DImage
 from .psf_analysis import PSF, BeadExtractor
 
