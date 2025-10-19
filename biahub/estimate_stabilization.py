5fd93b347a76230227934114728d34df840377a3
Taylla Theodoro <tayllamth@gmail.com>
2025-10-19 00:05:08 -0700

    revert change

--- biahub/estimate_stabilization.py
+++ biahub/estimate_stabilization.py
@@ -1244,7 +1244,7 @@ def estimate_z_focus_per_position(
         shift = np.eye(4)
         # moving -> reference
         # Set the translation components of the transform
-        shift[0, 3] =  z_val - z_val_next
+        shift[0, 3] =  z_val_next - z_val 
         z_focus_shift.append(shift)
 
     transform = np.array(z_focus_shift)
