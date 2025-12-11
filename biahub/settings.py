48399f67e9a3fd6bd9d8c7a5fec4d26233dfe991
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2025-12-11 19:18:00 +0100

    Concatenate with OME-Zarr v0.5 and sharding (#104)
    * add zarrs and organize dependency groups
    
    * configurable sharding
    
    * update chunking test
    
    * use clean env helper
    https://github.com/czbiohub-sf/biahub/pull/96#issuecomment-2921169316
    
    * update example config
    
    * disable threading for the zarrs codec
    
    * test variable sharding in time
    
    * print the correct cluster name
    
    * allow blocking
    
    * fix typing
    
    * wip: test values of the concatenated array
    
    * fix monitoring
    
    * remove zarrs codec
    
    * tweak resource estimation
    
    * block in testing
    
    * require tensorstore
    
    * update dependency groups
    
    * combine context managers
    
    * ultrack lazy import
    
    * style
    
    * point to the pre-release
    
    * raise error is trying to concatenate zarr stores with pyramids
    
    ---------
    
    Co-authored-by: Ivan Ivanov <ivan.ivanov@czbiohub.org>
    Co-authored-by: Sricharan Reddy Varra <sricharan.varra@czbiohub.org>

--- biahub/settings.py
+++ biahub/settings.py
@@ -373,7 +373,9 @@ class ConcatenateSettings(MyBaseModel):
     Y_slice: Union[list, list[Union[list, Literal["all"]]], Literal["all"]] = "all"
     Z_slice: Union[list, list[Union[list, Literal["all"]]], Literal["all"]] = "all"
     chunks_czyx: Union[Literal[None], list[int]] = None
+    shards_ratio: list[int] | None = None
     ensure_unique_positions: Optional[bool] = False
+    output_ome_zarr_version: Literal["0.4", "0.5"] = "0.4"
 
     @field_validator("concat_data_paths")
     @classmethod
